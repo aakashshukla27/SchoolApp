9acc02a41d3aa02cd79f9ed6c2258e4e8bfb6e32
Aakash Shukla <shukla.aa@northeastern.edu>
2022-06-12 23:37:14 -0400

    Added new interfaces

---
+++ neu.csye.dal/Entities/Application.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neu.csye.dal.Entities
+{
+    public class Application
+    {
+        public int ApplicationId { get; set; } // PK
+        public int ApplicantId { get; set; } // FK
+        public int GradeId { get; set; } // FK
+        public int ApplicationStatusId { get; set; } // FK
+        public DateTime ApplicationDate { get; set; }
+        public DateTime ApplicationModifiedDate { get; set; }
+        public int SchoolYear { get; set; }
+
+        /// <summary>
+        /// Each Application will belong to a specific applicant
+        /// </summary>
+        public Applicant Applicant { get; set; }
+
+        /// <summary>
+        /// Each application is applying for a specific grade
+        /// </summary>
+        public Grade Grade { get; set; }
+
+        /// <summary>
+        /// Each application will be in a status at a time
+        /// </summary>
+        public ApplicationStatus ApplicationStatus { get; set; }
+
+    }
+}
---
+++ neu.csye.dal/Functions/Interfaces/ICrud.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neu.csye.dal.Functions.Interfaces
+{
+    public interface ICrud
+    {
+        Task<T> Create<T>(T dbObject) where T : class;
+        Task<T> Update<T>(int entityId, T dbObject) where T : class;
+        Task<T> Read<T>(int entityId) where T : class;
+        Task<List<T>> ReadAll<T>() where T : class;
+        Task<bool> Delete<T>(int entityId) where T : class;
+    }
+}
