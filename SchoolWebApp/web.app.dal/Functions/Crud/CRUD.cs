2b8783f74aeab623588f6f4b3c429080ab57aeb9
Aakash Shukla <shukla.aa@northeastern.edu>
2023-02-07 22:38:54 -0500

    Added generic implementations

---
+++ SchoolWebApp/web.app.dal/Functions/Crud/CRUD.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using web.app.dal.DataContext;
+
+namespace web.app.dal.Functions.Crud
+{
+    internal class CRUD
+    {
+        #region CRUD
+        /// <summary>
+        /// Create a new record of type T.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="objectForDb"></param>
+        /// <returns></returns>
+        public async Task<T> Create<T>(T objectForDb) where T : class
+        {
+            try
+            {
+                using (var context = new DatabaseContext(DatabaseContext.Options.DatabaseOptions))
+                {
+                    await context.AddAsync<T>(objectForDb);
+                    await context.SaveChangesAsync();
+                    return objectForDb;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Get a record from the database of Type T, by passing the Id(Primary Key) of the object type you want back. 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entityId"></param>
+        /// <returns></returns>
+        public async Task<T> Read<T>(Int64 entityId) where T : class
+        {
+            try
+            {
+                using (DatabaseContext context = new DatabaseContext(DatabaseContext.Options.DatabaseOptions))
+                {
+                    T result = await context.FindAsync<T>(entityId);
+                    return result;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Gets all records from the database of Type T. 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns>Generic List Object</returns>
+        public async Task<List<T>> ReadAll<T>() where T : class
+        {
+            try
+            {
+                using (DatabaseContext context = new DatabaseContext(DatabaseContext.Options.DatabaseOptions))
+                {
+                    var result = await context.Set<T>().ToListAsync();
+                    return result;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Update a record in the database of Type T, by passing both updated value and the primary key. 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="objectToUpdate"></param>
+        /// <param name="entityId"></param>
+        /// <returns></returns>
+        public async Task<T> Update<T>(T objectToUpdate, Int64 entityId) where T : class
+        {
+            try
+            {
+                using (var context = new DatabaseContext(DatabaseContext.Options.DatabaseOptions))
+                {
+                    var objectFound = await context.FindAsync<T>(entityId);
+                    if (objectFound != null)
+                    {
+                        context.Entry(objectFound).CurrentValues.SetValues(objectToUpdate);
+                        await context.SaveChangesAsync();
+                    }
+                    return objectFound;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Delete a record of Type T from the database by passing the primary key value of the object you want deleted for the specified type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entityId"></param>
+        /// <returns></returns>
+        public async Task<bool> Delete<T>(Int64 entityId) where T : class
+        {
+            try
+            {
+                using (DatabaseContext context = new DatabaseContext(DatabaseContext.Options.DatabaseOptions))
+                {
+                    T recordToDelete = await context.FindAsync<T>(entityId);
+                    if (recordToDelete != null)
+                    {
+                        context.Remove(recordToDelete);
+                        await context.SaveChangesAsync();
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ SchoolWebApp/web.app.dal/Functions/Interfaces/IApplication_Operations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using web.app.dal.Entities;
+
+namespace web.app.dal.Functions.Interfaces
+{
+    public interface IApplication_Operations
+    {
+        Task<Application> AddFullApplication(Int64 gradeId, Int64 applicationStatusId, Int32 schoolYear, string firstName, string surname, DateTime birthDate, string email, string contactNumber);
+
+        Task<List<Application>> GetApplicationsByApplicantId(Int64 applicantId);
+    }
+}
